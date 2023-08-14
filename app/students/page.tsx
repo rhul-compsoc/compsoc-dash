686af6ee072f2ed381f06eda1cf123625f3fd1ad
joseph-beck <joebeck03@icloud.com>
2023-08-14 21:08:07 +0100

    add students page

---
+++ app/students/page.tsx
@@ -0,0 +1,14 @@
+import { Footer } from '@/components/footer';
+import { NavigationBar } from '@/components/navigationBar';
+
+export default function Home() {
+  return (
+    <main>
+      <NavigationBar currentPage="student"/>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>students</h1>
+      </div>
+      <Footer/>
+    </main>
+  );
+}
--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -83,6 +83,19 @@ export const SearchBar = ({ currentPage }: { currentPage: string }): JSX.Element
             </li>
             <li>
               <Link
+                href="students"
+                className={`${
+                  currentPage === 'students'
+                    ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                    : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                }`}
+                aria-current="page"
+              >
+                Students
+              </Link>
+            </li>
+            <li>
+              <Link
                 href="members"
                 className={`${
                   currentPage === 'members'
