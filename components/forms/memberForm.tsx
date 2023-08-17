36394209549c1db2cbab5021118974d88d638095
joseph-beck <joebeck03@icloud.com>
2023-08-17 22:31:50 +0100

    fix form not letting user fill

--- components/forms/memberForm.tsx
+++ components/forms/memberForm.tsx
@@ -1,35 +1,26 @@
 import { postMember } from "@/lib/api/members";
 import { Member } from "@/lib/types/member";
-import { useState } from "react";
 
 export const CreateForm = (): JSX.Element => {
-  const [member, setMember] = useState<Member>({
-    id: undefined,
-    student_id: '',
-    student_email: '',
-    first_name: '',
-    last_name: '',
-    active_member: false,
-  });
+  const onClick = async () => {
+    const idInput = document.getElementById("id") as HTMLInputElement;
+    const studentIdInput = document.getElementById("student-id") as HTMLInputElement;
+    const studentEmailInput = document.getElementById("student-email") as HTMLInputElement;
+    const firstNameInput = document.getElementById("first-name") as HTMLInputElement;
+    const lastNameInput = document.getElementById("last-name") as HTMLInputElement;
+    const activeMemberSelect = document.getElementById("membership-status") as HTMLSelectElement;
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { id, value, type } = event.target;
+    const member: Member = {
+      id: parseInt(idInput.value),
+      student_id: studentIdInput.value,
+      student_email: studentEmailInput.value,
+      first_name: firstNameInput.value,
+      last_name: lastNameInput.value,
+      active_member: activeMemberSelect.value === "True",
+    };
 
-    const newValue = type === 'checkbox' ? (event.target as HTMLInputElement).checked : value;
-
-    setMember((prevMember) => ({
-      ...prevMember,
-      [id]: newValue,
-    }));
-  };
-
-  const handleSubmit = async () => {
-    try {
-      const resp = await postMember(member);
-      console.log(resp);
-    } catch (error) {
-      console.error('Error posting member:', error);
-    }
+    const resp = await postMember(member);
+    console.log(resp);
   };
 
   return (
@@ -44,8 +35,6 @@ export const CreateForm = (): JSX.Element => {
         <input
           type="id"
           id="id"
-          value={member.id !== undefined ? member.id.toString() : ''}
-          onChange={handleInputChange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="123"
         />
@@ -61,8 +50,6 @@ export const CreateForm = (): JSX.Element => {
         <input
           type="id"
           id="student-id"
-          value={member.student_id}
-          onChange={handleInputChange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="0123456789"
           required
@@ -79,8 +66,6 @@ export const CreateForm = (): JSX.Element => {
         <input
           type="email"
           id="student-email"
-          value={member.student_email}
-          onChange={handleInputChange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="abcd123@live.rhul.ac.uk"
           required
@@ -97,8 +82,6 @@ export const CreateForm = (): JSX.Element => {
       <input
         type="text"
         id="first-name"
-        value={member.first_name}
-        onChange={handleInputChange}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="0123456789"
         required
@@ -115,8 +98,6 @@ export const CreateForm = (): JSX.Element => {
         <input
           type="text"
           id="last-name"
-          value={member.last_name}
-          onChange={handleInputChange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="0123456789"
           required
@@ -130,12 +111,7 @@ export const CreateForm = (): JSX.Element => {
         >
           Active Member
         </label>
-        <select
-          id="membership-status"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          value={member.active_member ? 'True' : 'False'}
-          onChange={handleInputChange}
-        >
+        <select id="membership-status" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           <option>False</option>
           <option>True</option>
         </select>
@@ -143,7 +119,7 @@ export const CreateForm = (): JSX.Element => {
       <button
         type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        onClick={handleSubmit}
+        onClick={onClick}
       >
         Submit
       </button>
