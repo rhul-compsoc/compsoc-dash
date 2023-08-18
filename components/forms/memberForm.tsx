dbd1bbdd4e096244330ce5870e135868832dd1fa
joseph-beck <joebeck03@icloud.com>
2023-08-18 16:04:16 +0100

    add edit form and respective requests

--- components/forms/memberForm.tsx
+++ components/forms/memberForm.tsx
@@ -1,5 +1,9 @@
-import { postMember } from "@/lib/api/members";
+import { patchMember, postMember } from "@/lib/api/members";
 import { Member } from "@/lib/types/member";
+
+interface FormProps {
+  member?: Member;
+}
 
 export const CreateForm = (): JSX.Element => {
   const onClick = async () => {
@@ -10,7 +14,7 @@ export const CreateForm = (): JSX.Element => {
     const lastNameInput = document.getElementById("last-name") as HTMLInputElement;
     const activeMemberSelect = document.getElementById("membership-status") as HTMLSelectElement;
 
-    const member: Member = {
+    const post: Member = {
       id: parseInt(idInput.value),
       student_id: studentIdInput.value,
       student_email: studentEmailInput.value,
@@ -19,12 +23,12 @@ export const CreateForm = (): JSX.Element => {
       active_member: activeMemberSelect.value === "True",
     };
 
-    const resp = await postMember(member);
+    const resp = await postMember(post);
     console.log(resp);
   };
 
   return (
-    <form className="grid grid-cols-1 md:grid-cols-3 gap-4">
+    <form>
       <div className="mb-6">
         <label
           htmlFor="id"
@@ -83,7 +87,7 @@ export const CreateForm = (): JSX.Element => {
         type="text"
         id="first-name"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="0123456789"
+        placeholder="first name"
         required
       />
       </div>
@@ -99,7 +103,7 @@ export const CreateForm = (): JSX.Element => {
           type="text"
           id="last-name"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="0123456789"
+          placeholder="last name"
           required
         />
       </div>
@@ -111,7 +115,10 @@ export const CreateForm = (): JSX.Element => {
         >
           Active Member
         </label>
-        <select id="membership-status" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <select
+          id="membership-status"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
           <option>False</option>
           <option>True</option>
         </select>
@@ -127,6 +134,138 @@ export const CreateForm = (): JSX.Element => {
   );
 };
 
-export const EditForm = () => {
+export const EditForm: React.FC<FormProps> = ({ member }): JSX.Element => {
+  const onClick = async () => {
+    const idInput = document.getElementById("id") as HTMLInputElement;
+    const studentIdInput = document.getElementById("student-id") as HTMLInputElement;
+    const studentEmailInput = document.getElementById("student-email") as HTMLInputElement;
+    const firstNameInput = document.getElementById("first-name") as HTMLInputElement;
+    const lastNameInput = document.getElementById("last-name") as HTMLInputElement;
+    const activeMemberSelect = document.getElementById("membership-status") as HTMLSelectElement;
 
+    const patch: Member = {
+      id: parseInt(idInput.value),
+      student_id: studentIdInput.value,
+      student_email: studentEmailInput.value,
+      first_name: firstNameInput.value,
+      last_name: lastNameInput.value,
+      active_member: activeMemberSelect.value === "True",
+    };
+
+    const resp = await patchMember(patch);
+    console.log(resp);
+  };
+
+  return (
+    <form>
+      <div className="mb-6">
+        <label
+          htmlFor="id"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          ID
+        </label>
+        <input
+          type="text"
+          id="id"
+          aria-label="disabled input"
+          className="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={member?.id}
+          disabled
+        />
+      </div>
+
+      <div className="mb-6">
+        <label
+          htmlFor="student-id"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Student ID
+        </label>
+        <input
+          type="id"
+          id="student-id"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="0123456789"
+          defaultValue={member?.student_id}
+          required
+        />
+      </div>
+
+      <div className="mb-6">
+        <label
+          htmlFor="student-email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Student Email
+        </label>
+        <input
+          type="email"
+          id="student-email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="abcd123@live.rhul.ac.uk"
+          defaultValue={member?.student_email}
+          required
+        />
+      </div>
+
+      <div className="mb-6">
+      <label
+        htmlFor="first-name"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        First Name
+      </label>
+      <input
+        type="text"
+        id="first-name"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="first name"
+        defaultValue={member?.first_name}
+        required
+      />
+      </div>
+
+      <div className="mb-6">
+        <label
+          htmlFor="last-name"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Last Name
+        </label>
+        <input
+          type="text"
+          id="last-name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="last name"
+          defaultValue={member?.last_name}
+          required
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="membership-status"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Active Member
+        </label>
+        <select
+          id="membership-status"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          defaultValue={member?.active_member ? "True" : "False"}
+        >
+          <option>False</option>
+          <option>True</option>
+        </select>
+      </div>
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={onClick}
+      >
+        Submit
+      </button>
+    </form>
+  );
 };
